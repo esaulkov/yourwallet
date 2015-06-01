d09823fb7bbb55af9e4d540ed7d7860fa470188d
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-06-01 08:35:08 +0700

    Add categories controller test

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -4,5 +4,13 @@ FactoryGirl.define do
   factory :category do |f|
     f.name  Faker::Lorem.characters(30)
   end
+  
+  factory :other_category, class: Category do |f|
+    f.name  Faker::Lorem.characters(15)
+  end
+  
+  factory :invalid_category, class: Category do |f|
+    f.name  Faker::Lorem.characters(51)
+  end
 end
  
