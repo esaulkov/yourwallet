c7c89e3bb4c41f098a10b15b8756d053a16efc39
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-06-12 18:56:18 +0700

    Small fix _form partial in purchase view

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -67,7 +67,6 @@ class PurchasesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_purchase
       @purchase = Purchase.find(params[:id])
-#      @category = Category.find(@purchase.category_id)
     end
 
     def set_category
