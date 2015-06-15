8d56041cd292371d3730c2e06d6815d6beb4ec57
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-06-15 10:48:55 +0700

    Add wallet balance change on transactions actions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,13 +3,34 @@ class Transaction < ActiveRecord::Base
   belongs_to :purchase
 
   before_validation :get_absolute_sum
+  before_update     :save_transaction_sum
+  after_create      :add_to_wallet
+  after_update      :add_to_wallet
+  after_destroy     :remove_from_wallet
   
   validates_numericality_of :sum, greater_than_or_equal_to: 0.01
 #  validates_format_of :date_time, with:
   validates_presence_of :wallet_id
   validates_presence_of :purchase_id
   
-  def get_absolute_sum
-    self.sum = self.sum.abs
-  end
+  private
+    def get_absolute_sum
+      self.sum = self.sum.abs
+    end
+    
+    def add_to_wallet
+      wallet.increment(:balance, sum)
+      wallet.save
+    end
+    
+    def save_transaction_sum
+      @temp_trans = Transaction.find(id)
+      wallet.decrement(:balance, @temp_trans.sum)
+      wallet.save
+    end
+    
+    def remove_from_wallet
+      wallet.decrement(:balance, sum)
+      wallet.save
+    end
 end
