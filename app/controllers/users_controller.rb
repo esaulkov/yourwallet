83244b808d4f20b59279bb1c1a7c252109778f7b
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-08-20 12:35:28 +0700

    Fix Hound reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @last_transactions = current_user.get_last_transactions
-    flash.now[:alert] = t('.alert') if current_user.has_overdraft?
+    flash.now[:alert] = t(".alert") if current_user.has_overdraft?
   end
 end
  
